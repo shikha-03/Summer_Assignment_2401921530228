972c63724d716a30754db11970204a3c7b6290c3
shikha-03 <shikha99873@gmail.com>
2026-06-23 17:20:26 +0530

    Week 3

---
+++ Week_3/Day_5/number_of_recent_calls.cpp
@@ -0,0 +1,19 @@
+class RecentCounter {
+public:
+
+    queue<int> q;
+
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        int temp = t - 3000;
+        q.push(t);
+        while(q.front() < temp)
+        {
+            q.pop();
+        }
+        return q.size();
+    }
+};
